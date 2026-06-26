ce433d10a9f137eaca45538322b100cdc5b9b947
Térence Da Conceiçao <terence.daco@gmail.com>
2026-06-26 16:17:56 +0200

    feat: add vitest and check if it's working

---
+++ front-InkAndVoices/vitest.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
+
+export default defineConfig({
+  plugins: [react(), Pages({
+    dirs: 'src/pages',
+  })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
+  }
+})
