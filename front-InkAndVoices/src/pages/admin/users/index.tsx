44d19394573502360ceec47525288452f0d4a770
Térence Da Conceiçao <terence.daco@gmail.com>
2026-06-27 16:22:30 +0200

    feat: ajout de l'arborescence des pages

---
+++ front-InkAndVoices/src/pages/admin/users/index.tsx
@@ -0,0 +1,16 @@
+const dashboardAdmin = () => { 
+    return (
+        <>
+            <h1>Dashboard</h1>
+                <div>
+                    Ceci est un test de dashboard d'admin.
+                    <div>PROFIL</div>
+                    <div>BOOKS REVIEWS</div>
+                    <div>USERS</div>
+
+                </div>
+        </>
+    )
+}
+
+export default dashboardAdmin; 
