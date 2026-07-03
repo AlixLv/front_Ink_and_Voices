94a2c37d57546ee797b0cf29c570e6fa120ccf7c
Térence Da Conceiçao <terence.daco@gmail.com>
2026-07-04 01:16:36 +0200

    wip

--- front-InkAndVoices/src/pages/index.tsx
+++ front-InkAndVoices/src/pages/index.tsx
@@ -10,7 +10,7 @@
 // IL FAUT QUE CETTE PAGE SOIT ACCESSIBLE SANS AUTHENTIFICATION
 // ET AUSSI AVEC AUTHENTIFICATION MAIS EN RAJOUTANT DES COMPOSANTS
 
-import UserCardContainer from '../containers/userCardContainer';
+import UserCardContainer from '../containers/HomeHeaderContainer/HomeHeaderContainer';
 import RecentBooksContainer from '../containers/RecentBooksContainer/RecentBooksContainer';
 import '../containers/RecentBooksContainer/RecentBooksContainer.css'; // obligé d'importer le css ici car on importe des containers, pas des composants, donc le css doit être appelé dans le coposant où il est utilisé... et ça doit être index.tsx. A voir si on rajoute juste ce titre dans index.css ou si on fait une page à part (au cas où il y aie d'autres styles sur index.tsx)
 export default function Homepage() {
