fd47be077eadb6ab9409dc7d5238357f20c6ed6c
Térence Da Conceiçao <terence.daco@gmail.com>
2026-07-04 23:58:18 +0200

    rajout de vite.config et tests

---
+++ front-InkAndVoices/src/components/SubmitButton/SubmitButton.test.tsx
@@ -0,0 +1,27 @@
+// import { describe, it, expect, vi } from 'vitest';
+// import { render, screen } from '@testing-library/react';
+// import SubmitButton from './SubmitButton';
+// import { BrowserRouter } from 'react-router-dom';
+
+// describe('SubmitButton Component', () => {
+//   it('should render a button with correct text', () => {
+//     render(
+//       <BrowserRouter>
+//         <SubmitButton route="/add-book" text="Ajouter un livre" />
+//       </BrowserRouter>
+//     );
+    
+//     const button = screen.getByRole('button', { name: /Ajouter un livre/i });
+//     expect(button).toBeInTheDocument();
+//   });
+
+//   it('should have correct button text content', () => {
+//     render(
+//       <BrowserRouter>
+//         <SubmitButton route="/home" text="Go Home" />
+//       </BrowserRouter>
+//     );
+    
+//     expect(screen.getByText('Go Home')).toBeInTheDocument();
+//   });
+// });
