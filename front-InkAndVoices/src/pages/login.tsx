e43714a51afe4a5f7d31cfd15fd339ffbaf7660b
Térence Da Conceiçao <terence.daco@gmail.com>
2026-07-07 21:03:15 +0200

    add logo

--- front-InkAndVoices/src/pages/login.tsx
+++ front-InkAndVoices/src/pages/login.tsx
@@ -1,1 +1,20 @@
-<div>LOGIN</div>
+import LogoContainer from "../components/LogoContainer/LogoContainer";
+
+export default function Login() {
+  return (
+    <>
+        <LogoContainer />
+    </>
+  )
+}
+
+
+
+
+
+
+
+
+// Je dois mettre un formulaire, donc créer des composants.
+// Ensuite, les champs de ces formulaires vont utiliser des hooks liés à un Login Service qui va executer une requête POST.
+// Ca va vérifier si c'est identique à ce qu'il y a en DB (truc statique pour l'instant) et si c'est ok, ça connecte. Ca renvoie un JWT.
