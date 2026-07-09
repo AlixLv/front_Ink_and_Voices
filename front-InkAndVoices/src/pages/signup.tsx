22bd9b8c3f603b6fc19d8e6a516f74b07b0ea8df
Térence Da Conceiçao <terence.daco@gmail.com>
2026-07-09 18:07:55 +0200

    authform

--- front-InkAndVoices/src/pages/signup.tsx
+++ front-InkAndVoices/src/pages/signup.tsx
@@ -1,13 +1,22 @@
+// import SignUpForm from '../components/SignUpForm/SignUpForm';
+
+// export default function Authenticate() {
+//     return (
+//         <>
+//             <h1>Se créer un compte</h1>
+//             <SignUpForm />
+//         </>
+//     )
+// }
+
+// src/pages/signup.tsx
+import AuthPageUI from '../components/AuthPageUI/AuthPageUI';
 import SignUpForm from '../components/SignUpForm/SignUpForm';
 
-// rajouter le css
-// il faut refactorer en un seul form le login et le signup (sinon c'ets pas react logic)
-
-export default function Authenticate() {
-    return (
-        <>
-            <h1>Se créer un compte</h1>
-            <SignUpForm />
-        </>
-    )
+export default function Signup() {
+  return (
+    <AuthPageUI>
+      <SignUpForm />
+    </AuthPageUI>
+  )
 }
