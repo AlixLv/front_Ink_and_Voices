beb44d2ecce0b91b90da6718699af9ac315c1c2a
Térence Da Conceiçao <terence.daco@gmail.com>
2026-07-09 17:23:42 +0200

    feat: add UI for authContainer

---
+++ front-InkAndVoices/src/components/AuthFormContainer/AuthFormContainer.tsx
@@ -0,0 +1,15 @@
+// import LoginForm ou SignUpForm
+import LogoContainer from "../LogoContainer/LogoContainer";
+// import SignUpForm from "../SignUpForm/SignUpForm";
+
+export default function AuthForm() {
+  return (
+    <>
+        <LogoContainer />
+        {/* <LoginForm /> ou <SignUpForm */}
+    </>
+  )
+}
+
+
+
