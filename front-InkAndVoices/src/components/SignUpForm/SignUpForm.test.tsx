0ab71c0e9a57ea53a7830f48b61fb6bb57c272d1
Térence Da Conceiçao <terence.daco@gmail.com>
2026-07-09 17:09:59 +0200

    cleaned comments

--- front-InkAndVoices/src/components/SignUpForm/SignUpForm.test.tsx
+++ front-InkAndVoices/src/components/SignUpForm/SignUpForm.test.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
 
 
-// ✅ Mocks au top-level
+// mocks
 const mockNavigate = vi.fn();
 vi.mock('react-router-dom', async () => {
     const actual = await vi.importActual('react-router-dom');
