37de81c7d9bf8ec4578bcd637ee187aa73bf094b
Alix <alix.leve@gmail.com>
2026-07-10 10:37:12 +0200

    feature: add getSingleBook service

--- front-InkAndVoices/src/services/BooksService.tsx
+++ front-InkAndVoices/src/services/BooksService.tsx
@@ -1,4 +1,5 @@
 import type { Book } from '../types/Book';
+
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8032';
 
 export const getBooks = async(): Promise<Book[]> => {
@@ -8,9 +9,26 @@ export const getBooks = async(): Promise<Book[]> => {
       throw new Error(`HTTP error. status: ${response.status}`);
     }
     const data = await response.json();
+    console.log("🌈 data: ", data)
     return data;
   } catch(error) {
       console.error("Erreur lors de la récupération de l'ensemble des livres", error)
     return [];
   }
 }
+
+export const getSingleBook = async(id: number): Promise<Book> => {
+  try {
+    const response = await fetch(`${API_URL}api/book/${id}`, { method: 'GET' });
+    if(!response.ok){
+      throw new Error(`HTTP error. status: ${response.status}`);
+    }
+    const bookData = await response.json();
+    console.log("book received 📙: ", bookData)
+    return bookData;
+  } catch(error){
+    console.error("Erreur lors de la récupération du livre", error)
+    throw error;
+  }
+}
+
